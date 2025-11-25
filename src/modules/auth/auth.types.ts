1ab14b988eaf2835ebbd707681b78ce28aae3e95
Maxim Egorov <egorov.ms@lynxsoft.ru>
2025-11-25 13:45:56 +0700

    Фикс auth.types.ts

--- src/modules/auth/auth.types.ts
+++ src/modules/auth/auth.types.ts
@@ -1,24 +1,6 @@
-import { Request, Response, NextFunction } from 'express';
-import { User } from 'users/user.types';
-
-// Типы для стратегий аутентификации
-export type VerifyCallback = (
-    error: any,
-    user?: User | false,
-    options?: { message: string }
-) => void;
-
-export type LocalStrategyVerify = (
-    email: string,
-    password: string,
-    done: VerifyCallback
-) => Promise<void>;
-
-export type AuthenticateCallback = (
-    err: any,
-    user?: User | false,
-    info?: { message: string }
-) => void;
-
-export type AuthMiddleware = (req: Request, res: Response, next: NextFunction) => void;
-export type RoleMiddleware = (roles: string[]) => AuthMiddleware;
+export interface OAuthProfile {
+  id: string;
+  displayName: string;
+  emails?: Array<{ value: string }>;
+  photos?: Array<{ value: string }>;
+}
